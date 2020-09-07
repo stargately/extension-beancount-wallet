4ec265247fb5a3d9b4d2fd7101cd6ee95c19f23f
Tian Pan <puncsky@gmail.com>
2020-09-06 21:04:32 -0700

    add ava test

---
+++ ava.config.js
@@ -0,0 +1,5 @@
+export default {
+  require: ["ts-node/register", "tsconfig-paths/register"],
+  files: ["src/**/*.test.ts"],
+  extensions: ["ts"],
+};
---
+++ src/wallet-core/__test__/wallet-core.test.ts
@@ -0,0 +1,5 @@
+import test from "ava";
+
+test("testname", async (t) => {
+  t.truthy(true);
+});
