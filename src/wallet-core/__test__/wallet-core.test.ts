75bb50ad799ae5f9ba204ee192aa65865ba66012
Tian Pan <puncsky@gmail.com>
2020-09-10 00:33:00 -0700

    introduce eth-keyring-controller as dev deps

--- src/wallet-core/__test__/wallet-core.test.ts
+++ src/wallet-core/__test__/wallet-core.test.ts
@@ -1,5 +1,29 @@
 import test from "ava";
 
-test("testname", async (t) => {
-  t.truthy(true);
+const KeyringController = require("eth-keyring-controller");
+require("browser-env")();
+const { Crypto } = require("@peculiar/webcrypto");
+
+// @ts-ignore
+if (!global.crypto) {
+  // @ts-ignore
+  global.crypto = new Crypto();
+}
+
+test("lock keys", async (t) => {
+  const keyringController = new KeyringController({});
+  keyringController.createNewVaultAndKeychain("yo");
+  const privateKey =
+    "98ba3472fce96b0135e7ad7923a0c6f9ee8ec98a039529752a3a6e4d43bc802a";
+  const keyring = await keyringController.addNewKeyring("Simple Key Pair", [
+    privateKey,
+  ]);
+  t.deepEqual(keyring.wallets[0]._privKey.toString("hex"), privateKey);
+  // verify key added and converted
+  t.notDeepEqual(keyringController.keyrings[0], keyring);
+
+  // lock keys
+  t.deepEqual(keyringController.keyrings.length, 1);
+  await keyringController.setLocked();
+  t.deepEqual(keyringController.keyrings.length, 0);
 });
