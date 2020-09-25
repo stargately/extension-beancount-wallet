70ef14acc547c2c78dec641e10c05a71d5f2015b
icyfanfan <memory.icy@gmail.com>
2020-09-25 17:03:41 +0800

    welcome component

---
+++ src/components/Greetings/Welcome.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { themeDecorator } from "../../storybookUtils";
+import { Welcome } from "./Welcome";
+
+export const Standard: React.FC = () => <Welcome />;
+
+export default {
+  title: "Components/Welcome",
+  decorators: [themeDecorator()],
+  component: Welcome,
+};
