b86eee4fb756e4bf43a4ce0a9624624bbfaef273
Tian Pan <puncsky@gmail.com>
2020-10-07 00:56:44 -0700

    preserve state across tags openings and closes

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,5 +1,6 @@
 import "../../assets/img/icon-34.png";
 import "../../assets/img/icon-128.png";
+import { ExtensionStore } from "../../storage/local-store";
 
 console.log("This is the background page.");
 console.log("Put the background scripts here.");
@@ -7,3 +8,17 @@ console.log("Put the background scripts here.");
 chrome.runtime.onInstalled.addListener(() => {
   console.log("App installed");
 });
+
+const localStore = new ExtensionStore();
+const isDev = process.env.NODE_CONFIG_ENV === "dev";
+if (isDev) {
+  global.getState = localStore.get.bind(localStore);
+}
+
+async function initialize() {
+  const initState = await localStore.get();
+  console.log("initState", initState);
+  console.debug("Beacount wallet initialization complete.");
+}
+
+initialize().catch(console.error);
