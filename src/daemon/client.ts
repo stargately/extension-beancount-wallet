879036f5545179f1c9cd9e9498fa3187b8f68653
kwoktung <onlyif2009@gmail.com>
2020-10-14 22:35:37 +0800

    refactor: make wallet run in background

--- src/daemon/client.ts
+++ src/daemon/client.ts
@@ -1,9 +1,15 @@
 // Encapsulation all type request
 import { Client } from "./agent";
 import { GET_STATE, SET_STATE } from "./store";
-import { CREATE_PASSWORD, WALLET_UNLOCK } from "./wallet";
+import {
+  CREATE_PASSWORD,
+  WALLET_UNLOCK,
+  WALLET_LOCK,
+  WALLET_INITIIATED,
+  WALLET_LOCKED,
+} from "./wallet";
 
-export default class ClientAgent {
+export default class DaemonClient {
   agent: Client;
 
   constructor(public port: chrome.runtime.Port) {
@@ -19,10 +25,34 @@ export default class ClientAgent {
   }
 
   async createPassword(password: string) {
-    await this.agent.sendRequest(CREATE_PASSWORD, password);
+    await this.agent.sendRequest<void>(CREATE_PASSWORD, password);
   }
 
   async walletUnlock(password: string) {
     await this.agent.sendRequest(WALLET_UNLOCK, password);
   }
+
+  async walletLock() {
+    await this.agent.sendRequest(WALLET_LOCK);
+  }
+
+  async walletLocked() {
+    return this.agent.sendRequest<boolean>(WALLET_LOCKED);
+  }
+
+  async walletInitiated() {
+    return this.agent.sendRequest<boolean>(WALLET_INITIIATED);
+  }
+}
+
+let _client: DaemonClient;
+
+export function getSingleton(client?: DaemonClient): DaemonClient {
+  if (_client) {
+    return _client;
+  }
+  if (client) {
+    _client = client;
+  }
+  return client!;
 }
