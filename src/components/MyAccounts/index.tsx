c63140a42ef2f8da9dfa27b55c1a8bee29c4b0d3
kwoktung <onlyif2009@gmail.com>
2020-10-28 23:30:12 +0800

    feature: delay lock wallet when popup exit (#56)

--- src/components/MyAccounts/index.tsx
+++ src/components/MyAccounts/index.tsx
@@ -1,11 +1,13 @@
 import React, { useCallback } from "react";
 import { useRecoilState } from "recoil";
+import { useHistory } from "react-router-dom";
 
 import { MyAccounts as Account } from "./MyAccounts";
 import { accountsList, accountAddress } from "../../recoil";
 import { clientSingleton } from "../../daemon/client";
 
 export const MyAccounts = () => {
+  const history = useHistory();
   const [accounts, setAccount] = useRecoilState(accountsList);
   const [address, setAddress] = useRecoilState(accountAddress);
   const onCreateAccount = useCallback(async () => {
@@ -14,7 +16,11 @@ export const MyAccounts = () => {
     );
     const _accounts = await clientSingleton.walletGetAccounts();
     setAccount(_accounts);
-  }, []);
+  }, [accounts]);
+  const onLock = async () => {
+    await clientSingleton.walletLock();
+    history.push("/");
+  };
 
   return (
     <Account
@@ -22,6 +28,7 @@ export const MyAccounts = () => {
       address={address}
       onAddAccount={onCreateAccount}
       onClickAccount={setAddress}
+      onLock={onLock}
     ></Account>
   );
 };
